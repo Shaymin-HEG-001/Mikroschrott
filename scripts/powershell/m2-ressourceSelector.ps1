bdaee6aba5c7d09bb7a52af13717e86c3d36464f
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-04-24 19:02:11 +0200

    Added new Wallpapers

--- scripts/powershell/m2-ressourceSelector.ps1
+++ scripts/powershell/m2-ressourceSelector.ps1
@@ -1,2 +1,3 @@
 # Copy Video for Module 2
 copy-item "${location}\extra\vid\vid${video}.mkv" "${_destinationPath}\module-2\video.mkv" -Force
+copy-item "${location}\extra\img\img${image}.bmp" "${_destinationPath}\module-2\image.bmp" -Force
--- system/module-2/conf.ps1
+++ system/module-2/conf.ps1
@@ -1,3 +1,4 @@
 # Configuration for Module 2
 $autoRun = $true
-$video = 2
+$video = 2 # Video to load from \extra\vid\
+$image = 2 # Image to load from \extra\img\

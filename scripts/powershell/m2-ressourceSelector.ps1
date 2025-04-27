b57569dfa7311c0d2a79c8ca4e4c0523045a963f
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-04-27 13:56:04 +0200

    Add limited Range Option for random Video/Image

--- scripts/powershell/m2-ressourceSelector.ps1
+++ scripts/powershell/m2-ressourceSelector.ps1
@@ -1,6 +1,16 @@
 # Copy Video for Module 2
 if ($video -eq -1) { $video = Get-Random -Minimum 0 -Maximum 5 }
+elseif ($video -is [array]) {
+    $indexv = Get-Random -Minimum 0 -Maximum $video.Length
+    $video = $video[$indexv]
+}
+
 if ($image -eq -1) { $image = Get-Random -Minimum 0 -Maximum 6 }
+elseif ($image -is [array]) {
+    $indexi = Get-Random -Minimum 0 -Maximum $image.Length
+    $image = $image[$indexi]
+}
+
 
 copy-item "${location}\extra\vid\vid${video}.mkv" "${_destinationPath}\module-2\video.mkv" -Force
 copy-item "${location}\extra\img\img${image}.bmp" "${_destinationPath}\module-2\image.bmp" -Force
--- system/module-2/conf.ps1
+++ system/module-2/conf.ps1
@@ -1,4 +1,4 @@
 # Configuration for Module 2
 $autoRun = $true
-$video = -1 # Video to load from \extra\vid\  Set to -1 for random
-$image = -1 # Image to load from \extra\img\  Set to -1 for random
+$video = -1 # Video to load from \extra\vid\  Set to -1 for random, if an array is used a random value will be picked
+$image = -1 # Image to load from \extra\img\  Set to -1 for random, if an array is used a random value will be picked

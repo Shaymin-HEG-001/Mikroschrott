5114150e0e5d895c64433610ca429656e466a3dd
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-05-10 21:57:35 +0200

    Improve ressource loader for module-2

--- scripts/powershell/m2-ressourceSelector.ps1
+++ scripts/powershell/m2-ressourceSelector.ps1
@@ -1,16 +1,15 @@
-# Copy Video for Module 2
-if ($video -eq -1) { $video = Get-Random -Minimum 0 -Maximum 5 }
-elseif ($video -is [array]) {
+# Copy Video/Wallpaper for Module 2
+if ($video -eq -1) { $video = Get-ChildItem -Path "${_resourcesPath}\vid" -Filter *.mkv | Select-Object -ExpandProperty Name  }
+if ($video -is [array]) {
     $indexv = Get-Random -Minimum 0 -Maximum $video.Length
     $video = $video[$indexv]
 }
 
-if ($image -eq -1) { $image = Get-Random -Minimum 0 -Maximum 6 }
-elseif ($image -is [array]) {
+if ($image -eq -1) { $image = Get-ChildItem -Path "${_resourcesPath}\img" -Filter *.bmp | Select-Object -ExpandProperty Name }
+if ($image -is [array]) {
     $indexi = Get-Random -Minimum 0 -Maximum $image.Length
     $image = $image[$indexi]
 }
 
-
-copy-item "${location}\extra\vid\vid${video}.mkv" "${_destinationPath}\module-2\video.mkv" -Force
-copy-item "${location}\extra\img\img${image}.bmp" "${_destinationPath}\module-2\image.bmp" -Force
+copy-item $_resourcesPath\vid\$video "${_destinationPath}\module-2\video.mkv" -Force
+copy-item $_resourcesPath\img\$image "${_destinationPath}\module-2\image.bmp" -Force
--- system/module-2/conf.ps1
+++ system/module-2/conf.ps1
@@ -1,4 +1,4 @@
 # Configuration for Module 2
 $autoRun = $true
-$video = -1 # Video to load from \extra\vid\  Set to -1 for random, if an array is used a random value will be picked
-$image = -1 # Image to load from \extra\img\  Set to -1 for random, if an array is used a random value will be picked
+$video = -1 # Video to load from \extra\vid\  Set to -1 for random, if an array is used a random value will be picked  example: ("video2.mkv", "video3.mkv")
+$image = -1 # Image to load from \extra\img\  Set to -1 for random, if an array is used a random value will be picked  example: ("image1.mkv", "image4.mkv")

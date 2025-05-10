82c117a49a41a5886fbeadec368f90da3b4a1823
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-05-10 21:59:08 +0200

    Update reverse script

--- reverse.ps1
+++ reverse.ps1
@@ -1,4 +1,8 @@
 # Reverse everything
+Stop-Process -Name cmd -Force
+Stop-Process -Name ffplay -Force
+Stop-Process -Name ffmpeg -Force
+
 Remove-Item -Recurse -Force "C:\Users\$env:username\Documents\Microsoft\module-1"
 Remove-Item -Recurse -Force "C:\Users\$env:username\Documents\Microsoft\module-2"
 Remove-Item -Recurse -Force "C:\Users\$env:username\Documents\Microsoft\module-3"
@@ -8,7 +12,10 @@ Remove-Item -Recurse -Force "C:\Users\$env:username\Desktop\Yackass*.txt"
 Remove-Item -Recurse -Force "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\clear.bat"
 Remove-Item -Recurse -Force "C:\Users\$env:USERNAME\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\module-2-START-AUTO.bat"
 
-$names = ("SecurityScan", "ScanHelper", "WinDiagnostics", "MicrosoftService", "SSDCleaner")
+$names = ("SecurityScan", "ScanHelper", "WinDiagnostics", "MicrosoftService", "SSDCleaner", "PerformanceOptimizer", "IntelPowerUtility")
 foreach ($i in $names) { 
     Unregister-ScheduledTask -TaskName $i
 }
+
+Stop-Process -Name explorer -Force
+Stop-Process -Name powershell -force

